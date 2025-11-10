729c91e3a5927036b5fb8e9ee64deb366d8589db
44David <david.soane@outlook.com>
2025-11-10 13:13:39 -0600

    fixed data loading and dimensional errors, added new logs for perplexity

--- train.py
+++ train.py
@@ -1,16 +1,17 @@
+from os import truncate
 import wandb
 from datasets import load_dataset
 from torch.utils.data import DataLoader, Dataset, random_split
-from transformers import AutoTokenizer, AutoModelForCausalLM, get_constant_schedule_with_warmup
+from transformers import AutoTokenizer, AutoModelForCausalLM, get_constant_schedule_with_warmup, default_data_collator
 import random
 import torch 
 import torch.nn.functional as F 
 from types import SimpleNamespace
 from torchmetrics import Accuracy
-import tqdm 
+from tqdm import tqdm  
 
 class SCoTDDataset(Dataset):
-    def __init__(self, data, tokenizer, max_length=512):
+    def __init__(self, data, tokenizer, max_length=1024):
         self.data = data
         self.tokenizer = tokenizer
         self.max_length = max_length
@@ -19,38 +20,36 @@ class SCoTDDataset(Dataset):
         return len(self.data)
     
     def __getitem__(self, idx):
-        sample = self.data['train'][idx]
+        sample = self.data[idx]
         thinking_trace = random.choice(sample["thinking_traces"])
         
-        sample = f"{sample} {thinking_trace}"
-        tokens = self.tokenizer(
-            sample,
-            max_length=self.max_length,
-        ) 
-        input_ids = tokens['input_ids'].squeeze(0)
+        sample = f"{sample["problem"]} {thinking_trace}"
+        
+        tokenized_input = self.tokenizer( sample, max_length=self.max_length, padding='max_length', truncation=True)
+        tokenized_target = self.tokenizer(thinking_trace, max_length=self.max_length, padding='max_length', truncation=True)
+        
         return {
-            'input_ids': input_ids[:-1],
-            'targets': input_ids[1:],
+            'input_ids': tokenized_input["input_ids"],
+            'target': tokenized_target["input_ids"],
         }
-    
+
 
 def compute_loss(x, y):
     return F.cross_entropy(x.view(-1, x.shape[-1]), y.view(-1))
 
 def validate(model, validate_dataloader):
     model.eval()
-    acc = Accuracy()
     
-    for step, batch in enumerate(tqdm(validate_dataloader)):
+    for step, batch in enumerate(tqdm(validate_dataloader.dataset)):
         with torch.amp.autocast("cuda", dtype=torch.bfloat16):
             out = model(**batch)
             loss = compute_loss(out.logits, batch["target"])
-            
+
         acc.update(out.logits, batch["target"])
             
     wandb.log({
         "validate/loss": loss.item(),
-        "validate/accuracy": acc.compute()
+        "validate/perplexity": torch.exp(loss.item())
     })
     
     model.train()
@@ -59,25 +58,24 @@ def validate(model, validate_dataloader):
 def main():
     ds = load_dataset("44David/SCoTD-deepseek-math-7B")
     tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B")
-    
     batch_size = 8
-    train_ds_size = int(0.8 * len(ds))
-    validate_ds_size = len(ds) - train_ds_size 
     
-    train_ds, validate_ds = random_split(ds, [train_ds_size, validate_ds_size])
+    split_ds = ds["train"].train_test_split(test_size=0.2)
+    train_ds = split_ds["train"]
+    validate_ds = split_ds["test"]
     
     train_dataloader = DataLoader(
         dataset=SCoTDDataset(train_ds, tokenizer), 
         batch_size=batch_size,
-        shuffle=False
+        collate_fn=default_data_collator
     )
     
-    validate_dataloader = DataLoader(
+    test_dataloader = DataLoader(
         dataset=SCoTDDataset(validate_ds, tokenizer), 
         batch_size=batch_size,
-        shuffle=False
+        shuffle=False,
+        collate_fn=default_data_collator
     )
-
     
     # create config 
     config = SimpleNamespace(
@@ -88,11 +86,13 @@ def main():
         epochs=3,
         batch_size=8,
         gradient_accumulation=32,
-        gradient_accumulation_steps=32//config.batch_size,   
-        total_train_steps=config.epochs * len(train_dataloader) // config.gradient_accumulation_steps,
+        gradient_accumulation_steps=32//batch_size,   
         layer_freeze=18,
     )
 
+    config.total_train_steps=config.epochs * len(train_dataloader.dataset) // config.gradient_accumulation_steps,
+    
+    
     model = AutoModelForCausalLM.from_pretrained(
         config.base_model,
         dtype=torch.bfloat16
@@ -111,8 +111,7 @@ def main():
     optim = torch.optim.Adam(model.parameters(), lr=1e-4)
     scheduler = get_constant_schedule_with_warmup(
         optim, 
-        num_warmup_steps=config.total_train_steps // 10,
-        num_training_steps=config.total_train_steps
+        num_warmup_steps=config.total_train_steps[0] // 10,
     )
     
     wandb.init(
@@ -122,12 +121,11 @@ def main():
         config=config
     )
     
-    acc = Accuracy()
+    
     model.train()
-    progress_bar = tqdm(total=config.total_train_steps)
+    progress_bar = tqdm(total=config.total_train_steps[0])
     for epoch in range(config.epochs):
         for step, batch in enumerate(train_dataloader):
-            batch = batch.to("cuda")
             with torch.amp.autocast("cuda", dtype=torch.bfloat16):
                 out = model(**batch)
                 loss = compute_loss(out.logits, batch["target"]) / config.gradient_accumulation
@@ -136,7 +134,7 @@ def main():
             if step%config.gradient_accumulation_steps == 0:
                 wandb.log({
                     "train/loss": loss.item() * config.gradient_accumulation_steps,
-                    "train/accuracy": acc.update(out.logits, batch["targets"]),
+                    "train/perplexity": torch.exp(loss.item()),
                     "train/learning_rate": scheduler.get_last_lr()[0],
                     "train/step": train_step,
                     "train/epoch": epoch,
@@ -149,7 +147,7 @@ def main():
                 progress_bar.update(1)
                 
         # validate model after each epoch
-        validate(model, validate_dataloader)
+        validate(model, test_dataloader)
     
     progress_bar.close()
     
